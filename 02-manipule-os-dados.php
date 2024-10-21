007eb82800ebbc05bac561958789b1d39f70b5c8
Joao Jorge <joaojorgedosanjos@gmail.com>
2024-10-21 08:35:01 -0300

    :book: study: implement class for course on php; exercise 02

---
+++ 02-manipule-os-dados.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Média notas do film";
+
+$filmName = "Sonic 3";
+$yearOfRelease = "2025";
+$sumRating = 9;
+$sumRating += 10;
+$sumRating += 8;
+$sumRating += 10;
+$averageRating = $sumRating / 4;
+$available = false;
+if ($available) {
+
+    $available = "sim";
+}
+else {$available = "não";}
+echo "Nome do filme: $filmName\nAno de lançamento: $yearOfRelease\nMédia de: $averageRating\nDisponivél no plano: $available\n";
